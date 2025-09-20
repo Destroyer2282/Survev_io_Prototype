5566dd278364dba108c1e4c9b07d695b4112cc68
dima <dimafedorcuk721@gmail.com>
2025-09-20 19:02:56 +0300

    guns fix

--- Assets/Scripts/GunShooting.cs
+++ Assets/Scripts/GunShooting.cs
@@ -3,26 +3,43 @@ using UnityEngine;
 public class GunShooting : MonoBehaviour
 {
     public ProjectileBehaviour ProjectilePrefab;
+
     [SerializeField] private Transform LaunchOffset;
     [SerializeField] private Transform Direction;
+
     public float bulletSpeed = 10f;
+    public float fireRate = 0.05f; // Интервал между выстрелами
+    public float damage;
+    private float nextFireTime = 0f;
+
+    [SerializeField] private AudioClip shoot;
+    private AudioSource src;
+    private void Start()
+    {
+        src = GetComponent<AudioSource>();
+        src.clip = shoot;
+    }
     private void Awake()
     {
         LaunchOffset = GameObject.Find("LaunchPos").transform;
         Direction = GameObject.Find("Direction").transform;
     }
+
     private void LateUpdate()
     {
         if (QuickSlots.Instance.usingGun)
         {
             QuickSlots.Instance.SetHandsPositionForGun();
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButton(0) && Time.time >= nextFireTime)
+            {
                 Shoot();
+                nextFireTime = Time.time + fireRate;
+            }
         }
     }
+
     private void Shoot()
     {
-        Debug.Log("Using SHOOT");
         if (ProjectilePrefab == null || LaunchOffset == null || Direction == null)
         {
             Debug.LogWarning("Shoot: ProjectilePrefab или LaunchOffset или Direction не задан.");
@@ -41,10 +58,9 @@ public class GunShooting : MonoBehaviour
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         Quaternion rot = Quaternion.Euler(0f, 0f, angle);
+        src.Play();
         ProjectileBehaviour bullet = Instantiate(ProjectilePrefab, LaunchOffset.transform.position, rot);
 
         bullet.SetDirection(direction, bulletSpeed);
-
-        //Debug.Log($"Shoot: dir={direction}, angle={angle}");
     }
 }
